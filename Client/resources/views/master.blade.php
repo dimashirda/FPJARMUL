fa2c765d4569dc95b93a95e0805f49b48300ad5e
dimashirda <dimas0308@gmail.com>
2017-12-11 12:11:52 +0700

    regis login

--- Client/resources/views/master.blade.php
+++ Client/resources/views/master.blade.php
@@ -5,6 +5,7 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 </head>
 <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -35,10 +36,14 @@
                 <a class="nav-link disabled" href="#">Disabled</a>
               </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
+            <form class="form-inline my-2 my-lg-0 col-md-6">
               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="searchhome">Search</button>
             </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="mr-sm-5"><a href="{{('/register')}}">Sign Up</a></li>
+              <li class="mr-sm-3"><a href="{{('/login')}}">Login</a></li>
+            </ul>
           </div>
       </nav>
 
