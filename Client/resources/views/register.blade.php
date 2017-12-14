47e268128056dd494c9536c06aa289a3ea0f712f
Unknown <hari14@mhs.if.its.ac.id>
2017-12-14 23:47:59 +0700

    Fixed view register

--- Client/resources/views/register.blade.php
+++ Client/resources/views/register.blade.php
@@ -6,47 +6,6 @@
 
 
 <div class="container" id="homecontent">
-
-        // Actions
-        function updateDisplay() {
-            var isChecked = $checkbox.is(':checked');
-
-            // Set the button's state
-            $button.data('state', (isChecked) ? "on" : "off");
-
-            // Set the button's icon
-            $button.find('.state-icon')
-                .removeClass()
-                .addClass('state-icon ' + settings[$button.data('state')].icon);
-
-            // Update the button's color
-            if (isChecked) {
-                $button
-                    .removeClass('btn-default')
-                    .addClass('btn-' + color + ' active');
-            }
-            else {
-                $button
-                    .removeClass('btn-' + color + ' active')
-                    .addClass('btn-default');
-            }
-        }
-
-        // Initialization
-        function init() {
-
-            updateDisplay();
-
-            // Inject the icon if applicable
-            if ($button.find('.state-icon').length == 0) {
-                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i>');
-            }
-        }
-        init();
-    });
-});
-</script>
-
 	<div class="container">
 
 <div class="row">
